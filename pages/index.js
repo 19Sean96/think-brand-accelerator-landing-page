db2229a45aa6232c1a82d4f7d242f2ed405ad287
Sean Alexander <1996.sean.alexander@gmail.com>
2021-04-06 10:18:31 -0700

    instantiated intersection observer

--- pages/index.js
+++ pages/index.js
@@ -8,63 +8,93 @@ import Footer from "../components/Sections/Footer";
 
 import { useState, useEffect } from "react";
 
-export default function Home() {
-	const [emailSuccess, setEmailSuccess] = useState(false);
+import { useInView } from 'react-intersection-observer'
 
-	const sendConfirmationEmail = async (data) => {
-		const res = await fetch("/api/sendConfirmation", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(data),
-		});
-		const response = await res.json(res);
-		console.log("HERE IS THE RES FROM THE EMAIL", response);
-		if (response.success) {
-			setEmailSuccess(true);
-		}
-	};
+function debounce(fn, ms) {
+  let timer
+  return _ => {
+    clearTimeout(timer)
+    timer = setTimeout(_ => {
+      timer = null
+      fn.apply(this, arguments)
+    }, ms)
+  }
+}
+
+export default function Home() {
+  const [emailSuccess, setEmailSuccess] = useState(false);
+
+  const sendConfirmationEmail = async (data) => {
+    const res = await fetch("/api/sendConfirmation", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const response = await res.json(res);
+    console.log("HERE IS THE RES FROM THE EMAIL", response);
+    if (response.success) {
+      setEmailSuccess(true);
+    }
+  };
 
   useEffect(() => {
-    console.log("THIS IS PAGE SPEC (root)")
+    console.log("THIS IS PAGE SPEC (root)");
 
     if (window.ga) {
-      console.log("DATA LAYER", window.dataLayer)
+      console.log("DATA LAYER", window.dataLayer);
       if (location.hostname !== "localhost") {
         // window.ga('send', 'pageview', location.href)
         window.dataLayer.push({
-          event: 'pageview',
-          location: location.href
-        })
+          event: "pageview",
+          location: location.href,
+        });
       }
     }
-  }, [])
+  }, []);
 
-	return (
-		<div>
-			<Head>
-				<title>THINK Brand Accelerator: The place to go for brands to grow.</title>
+  return (
+    <div>
+      <Head>
+        <title>
+          THINK Brand Accelerator: The place to go for brands to grow.
+        </title>
 
-        <meta name="description" content="The place to go for brands to grow." />
+        <meta
+          name="description"
+          content="The place to go for brands to grow."
+        />
         <meta property="og:type" content="article" />
         <meta property="og:title" content="Brand Accelerator" />
-        <meta property="og:description" content="The place to go for brands to grow." />
+        <meta
+          property="og:description"
+          content="The place to go for brands to grow."
+        />
         {/* <meta property="og:url" content="https://campaigns.thinkpro.net/" /> */}
-        <meta property="og:image" content="https://www.thinkpro.net/wp-content/uploads/newHomepageAllServices.png" />
-        <meta property="og:image:secure_url" content="https://www.thinkpro.net/wp-content/uploads/newHomepageAllServices.png" />
+        <meta
+          property="og:image"
+          content="https://www.thinkpro.net/wp-content/uploads/newHomepageAllServices.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://www.thinkpro.net/wp-content/uploads/newHomepageAllServices.png"
+        />
         <meta property="og:image:width" content="1199" />
         <meta property="og:image:height" content="572" />
 
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main>
-				<Header />
-				<Landing />
-				<Details />
-				{/* <Summary /> */}
-				<ContactForm sendConfirmationEmail={sendConfirmationEmail} emailSuccess={emailSuccess}/>
-				<Footer />
-			</main>
-		</div>
-	);
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+yarn 
+      <main>
+        <Header />
+        <Landing />
+        <Details />
+        <Summary />
+        <ContactForm
+          sendConfirmationEmail={sendConfirmationEmail}
+          emailSuccess={emailSuccess}
+        />
+        <Footer />
+      </main>
+    </div>
+  );
 }
